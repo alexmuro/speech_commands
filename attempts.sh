588f2a3066240856c53f25205844d2550dc08c84
alex <am3081@gmail.com>
2017-12-05 16:41:24 -0500

    pytorch attemp

--- attempts.sh
+++ attempts.sh
@@ -11,18 +11,16 @@ python train.py \
 python train.py \
 	--background_volume .3 \
 	--window_size_ms 20 \
-	--how_many_training_steps 50000,10000 \
+	--how_many_training_steps 150000,100000 \
 	--eval_step_interval 1000 \
-	--summaries_dir ./vgg_a \
+	--summaries_dir ./vgg_e \
 	--train_dir ./vgg_e \
 	--data_dir ../gsk_train \
 	--batch_size 500
+	--model_architecture vgg_e
 	--save_step_interval 1000
 	
-python freeze.py \
---window_size_ms 20 \
---start_checkpoint=./log/conv.ckpt-35000 \
---output_file=./frozen/conv1-35000
+python freeze.py --window_size_ms 20 --start_checkpoint=./log/conv.ckpt-35000 --output_file=./frozen/conv1-35000
 
 python train.py --data_dir ../gsk_train --summaries_dir ./train --train_dir ./train --model_architecture vgg_a
 
